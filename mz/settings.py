41eb86932891923cfbfbc2ade606445f590e43e5
seoyun <dhn04100@gmail.com>
2023-11-06 12:05:32 +0900

    REFACTOR settings

--- mz/settings.py
+++ mz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '3.38.169.57', '3.38.169.57:8000']
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
